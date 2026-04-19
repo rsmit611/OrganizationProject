0cf83f47c74f94377df0877826757c5a82cf4c90
nexinex7829 <nexinex7829@gmail.com>
2026-04-19 12:40:30 -0400

    added DeleteList function to list which removes all its notes

--- source/OrganizationProject.Core/Entities/ApplicationManager.cs
+++ source/OrganizationProject.Core/Entities/ApplicationManager.cs
@@ -7,23 +7,23 @@ using System.Threading.Tasks;
 namespace OrganizationProject.Core.Entities
 {
 
-	public class ApplicationManager
-	{
-		public Note NoteManager { get; }
-		public CalendarModule CalendarManager { get; }
-		public ListModule ListManager { get; }
-		public TextModule TextManager { get; }
+    //public class ApplicationManager
+    //{
+    //    public Note NoteManager { get; }
+    //    public CalendarModule CalendarManager { get; }
+    //    public ListModule ListManager { get; }
+    //    public TextModule TextManager { get; }
 
 
-		public ApplicationManager()
-		{
-			NoteManager = new Note();
+    //    public ApplicationManager()
+    //    {
+    //        NoteManager = new Note("defaultName");
 
-			CalendarManager = new CalendarModule(NoteManager);
-			ListManager = ListManager(NoteManager);
-			TextManager = new TextModule();
+    //        CalendarManager = new CalendarModule(NoteManager);
+    //        ListManager = ListManager(NoteManager);
+    //        TextManager = new TextModule();
 
-		}
-	}
+    //    }
+    //}
 
 }
